cf65c9b60b9bf831ff889d28d672a3a50d4e6c60
Mobashir Monim <mobashir@techynaf.com>
2018-08-09 09:51:23 +0600

    Implement manual user creation

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Profile;
+
+class ProfilesController extends Controller
+{
+    public function create (Request $request)
+    {
+        $profile = new Profile;
+        $user = new User;
+
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        $profile->user_id = $user->id;
+        $profile->f_name = $request->f_name;
+        $profile->m_name = $request->m_name;
+        $profile->l_name = $request->l_name;
+        $profile->job_title = $request->job_title;
+        $profile->company = $request->company;
+        $profile->c_website = $request->company_website;
+        $profile->country = $request->country;
+        $profile->industry = $request->industry;
+        $profile->blog_url = $profile->blog_url;
+        $profile->save();
+
+        return redirect('/')->with('success', 'Welcome to Ecube!');
+    }
+}
--- database/migrations/2018_08_09_031237_create_events_table.php
+++ database/migrations/2018_08_09_031237_create_events_table.php
@@ -15,15 +15,6 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('f_name');
-            $table->string('m_name')->nullable();
-            $table->string('l_name');
-            $table->string('job_title');
-            $table->string('company');
-            $table->string('c_website');
-            $table->string('country');
-            $table->string('industry');
-            $table->string('blog_url');
             $table->timestamps();
         });
     }
