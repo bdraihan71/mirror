0274e341e0c5596ddeffc93a85b48a17945fc40c
Mobashir Monim <mobashir@techynaf.com>
2018-08-09 11:00:54 +0600

    Update css reference in app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,22 +3,27 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Events. Entertainment. Experience.">
+    <meta name="author" content="Techynaf">
+    <title>Ecube Entertainment</title>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="vendor/font-awesome/css/all.css" rel="stylesheet" type="text/css">
+    <!-- Animate CSS -->
+    <link href="vendor/animate/animate.min.css" rel="stylesheet">
+    <!-- Owl CSS -->
+    <link href="vendor/owl/css/owl.carousel.min.css" rel="stylesheet">
+    <!-- Fullpage CSS -->
+    <link href="vendor/fullpage/fullpage.min.css" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="icon" href="img/favicon.png">
+    <!-- Custom styles -->
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
