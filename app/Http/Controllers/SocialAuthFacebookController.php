ec1ff3bbc792d343b3786543a6ef2cd598e4747e
Mobashir Monim <mobashir@techynaf.com>
2018-08-10 14:26:29 +0600

    Implement profile creation using facebook login

--- app/Http/Controllers/SocialAuthFacebookController.php
+++ app/Http/Controllers/SocialAuthFacebookController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 use App\User;
 
 use Illuminate\Http\Request;
-use App\Services\SocialFacebookAccountService;
 use Socialite;
 use Auth;
+use App\Profile;
+
 class SocialAuthFacebookController extends Controller
 {
     public function redirect()
@@ -14,17 +15,38 @@ class SocialAuthFacebookController extends Controller
         return Socialite::driver('facebook')->redirect();
     }
 
-    public function callback(SocialFacebookAccountService $service)
+    public function callback()
     {
         $account = Socialite::driver('facebook')->user();
         $user = User::where('email', $account->email)->first();
-        dd($account);
 
         if ($user == null) {
             $user = new User;
             $user->email = $account->email;
             $user->password = bcrypt($account->id);
             $user->save();
+
+            $profile = new Profile;
+            $name = split(' ', $account->name);
+            $profile->f_name = $name[0];
+
+            if (sizeof($name) == 1) {
+                $profile->l_name = '---';
+            } elseif (sizeof($name) == 2) {
+                $profile->l_name = $name[1];
+            } else {
+                $profile->l_name = $name[sizeof($name) - 1];
+                $x = '';
+
+                for ($i = 1; $i <= sizeof($name) - 2; $i++) {
+                    $x = $x.$name[$i];
+                }
+
+                $profile->m_name = $x;
+            }
+
+            $profile->user_id = $user->id;
+            $profile->save();
 
             Auth::login($user);
         } else {
