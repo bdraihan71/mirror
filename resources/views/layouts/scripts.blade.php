104af162436162db58ee337243e1923b5fddb4d2
Mobashir Monim <mobashir@techynaf.com>
2018-08-21 21:28:55 +0600

    Include all scripts

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -112,3 +112,109 @@
 
 
 </script>
+
+<script>
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+        css3: true,
+        scrollingSpeed: 1000,
+        navigation: true,
+        slidesNavigation: true,
+        responsiveHeight: 330,
+        dragAndMove: true,
+        dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
+        controlArrows: false,
+        autoScrolling:false
+        });
+    });
+    
+    
+    
+    
+    $(document).ready(function(){
+
+        $('.partners-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+                autoplay:true
+            },
+            600:{
+                items:3,
+                nav:false,
+                autoplay:true
+            },
+            1000:{
+                items:6,
+                nav:true,
+                autoplay:true
+            }
+        }
+    });
+
+});
+
+
+$(document).ready(function () {
+      // Hide the div
+      $("#div1").hide();
+      // Show the div after 5s
+      $("#div1").delay(500).fadeIn(300);  
+  });
+</script>
+
+<script>
+    $(document).ready(function(){
+
+$(".filter-button").click(function(){
+    var value = $(this).attr('data-filter');
+    
+    if(value == "all")
+    {
+        //$('.filter').removeClass('hidden');
+        $('.filter').show('1000');
+    }
+    else
+    {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+        $(".filter").not('.'+value).hide('3000');
+        $('.filter').filter('.'+value).show('3000');
+        
+    }
+});
+
+if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+</script>
+
+<script>
+    
+    $(function() {
+            var selectedClass = "";
+            $(".fil-cat").click(function(){ 
+            selectedClass = $(this).attr("data-rel"); 
+         $("#portfolio").fadeTo(100, 0.1);
+            $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+        setTimeout(function() {
+          $("."+selectedClass).fadeIn().addClass('scale-anm');
+          $("#portfolio").fadeTo(300, 1);
+        }, 300); 
+            
+        });
+    });
+
+
+
+
+
+</script>
