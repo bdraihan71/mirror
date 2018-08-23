5c8fa2bcb1d17ee1877dfc47eb230596577bc243
Mobashir Monim <mobashir@techynaf.com>
2018-08-23 16:24:47 +0600

    Add partners

--- resources/views/tickets/show-all.blade.php
+++ resources/views/tickets/show-all.blade.php
@@ -9,11 +9,11 @@
                 <h3 class="page-title">EVENT TICKETS</h3>
             </div>
             <br><br>
-            <h4 class="text-center">Please use (Command + p) for mac and (Crtl + p) for other platforms to print the pdf of the ticket</h4>
-            <br><br>
             @if (count($tickets) == 0)
                 <h3 class="page-title text-center">You Have No Tickets Purchased</h3>
             @else
+                <h4 class="text-center">Please use (Command + p) for mac and (Crtl + p) for other platforms to print the pdf of the ticket</h4>
+                <br><br>
                 <table class="table">
                     <thead>
                         <tr>
