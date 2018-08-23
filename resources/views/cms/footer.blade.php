be15c6d64253310dc322e52197c6c1825639d24d
Mobashir Monim <mobashir@techynaf.com>
2018-08-23 16:43:40 +0600

    Fix footer editing option

--- resources/views/cms/footer.blade.php
+++ resources/views/cms/footer.blade.php
@@ -20,7 +20,11 @@
         <div class="row">
             <div class="col-md-3  text-right  form-label-control">Contact Info</div>
             <div class="col-md-7">
-                <input type="text" id="article-ckeditor" class="form-control" name="contact" placeholder="Contact Information">
+                <textarea id="contact" name="contact" cols="30" rows="12" class="form-control ckeditor"></textarea>
+                <script type="text/javascript">
+                    CKEDITOR.replace( 'contact' );
+                    CKEDITOR.add            
+                </script>
             </div>
         </div>
 
