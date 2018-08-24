ce3d46225942dd964c98ede240d05b85b0de8076
Mobashir Monim <mobashir@techynaf.com>
2018-08-24 14:24:35 +0600

    Fix error

--- resources/views/analytics/event.blade.php
+++ resources/views/analytics/event.blade.php
@@ -39,7 +39,7 @@
                             {{$question->question}}
                         </div>
                         <div class="col-md-6 h-100 bor-b">
-                            {{$question->answers->where('user_id', $ticket->user_id)}}
+                            {{$question->answers->where('user_id', $ticket->user_id)->first()}}
                         </div>
                     </div>
                 @endforeach
