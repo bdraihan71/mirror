0273de695ae030fa6eba016c56cb1ce74fe227b4
Mobashir Monim <mobashir@techynaf.com>
2018-08-24 19:41:09 +0600

    Fix scroll error

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -38,15 +38,10 @@
     
     (function($){
         window.onbeforeunload = function(e){    
-        window.name += ' [' + $(window).scrollTop().toString() + '[' + $(window).scrollLeft().toString();
+            window.name += ' [' + location.pathname + '[' + $(window).scrollTop().toString() + '[' + $(window).scrollLeft().toString();
         };
         $.maintainscroll = function() {
-        if(window.name.indexOf('[') > 0)
-        {
-        var parts = window.name.split('['); 
-        window.name = $.trim(parts[0]);
-        window.scrollTo(parseInt(parts[parts.length - 1]), parseInt(parts[parts.length - 2]));
-        }   
+            if( parts[parts.length - 3] == location.pathname ){ window.scrollTo(parseInt(parts[parts.length - 1]), parseInt(parts[parts.length - 2])); }   
         };  
         $.maintainscroll();
     })(jQuery);
