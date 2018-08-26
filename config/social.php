46017d1433957237bed4ad3a2a64aa3c0944bce3
Mobashir Monim <mobashir@techynaf.com>
2018-08-26 11:01:10 +0600

    Put social media links

---
+++ config/social.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "FACEBOOK_LINK" => env('FACEBOOK_LINK', '#'),
+    "TWITTER_LINK" => env('TWITTER_LINK', '#'),
+    "YOUTUBE_LINK" => env('YOUTUBE_LINK', '#'),
+    "INSTAGRAM_LINK" => env('INSTAGRAM_LINK', '#'),
+    "GOOGLE_LINK" => env('GOOGLE_LINK', '#'),
+];
