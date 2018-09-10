848584a83e6622be012013544e9b2740eb8b54cd
Mobashir Monim <mobashir@techynaf.com>
2018-09-10 15:52:13 +0600

    Correct nav logo link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,7 @@
                     @endif
                 @endif
                 <div class="container">
-                    <a class="navbar-brand" href="index.html"><img class="logo" src="/frontend/img/logo.svg" alt="Logo"></a>
+                    <a class="navbar-brand" href="/"><img class="logo" src="/frontend/img/logo.svg" alt="Logo"></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,7 +2,7 @@
     <header id="home">
         <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
             <div class="container">
-                <a class="navbar-brand" href="index.html"><img class="logo" src="/frontend/img/logo.svg" alt="Logo"></a>
+                <a class="navbar-brand" href="/"><img class="logo" src="/frontend/img/logo.svg" alt="Logo"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                     aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
