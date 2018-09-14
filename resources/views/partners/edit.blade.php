2240876f0372638e1e225b59914446cfce69cc54
Mobashir Monim <mobashir@techynaf.com>
2018-09-14 23:54:13 +0600

    Correct image resolution of partners

--- resources/views/partners/edit.blade.php
+++ resources/views/partners/edit.blade.php
@@ -39,7 +39,7 @@
                                 <label>Partner Logo *</label>
                             </div>
                             <div class="form-group col-md-10">
-                                <div>Please upload images with ratio of 3:2, e.g. 1920 x 1280 | max image size is 2 MB</div>
+                                <div>Please upload image of 231 x 231 | max image size is 2 MB</div>
                                 <input type="file" class="form-control" name="url">
                                 <img src="{{$partner->img}}" class="img-fluid" alt="Partner logo">
                             </div>
