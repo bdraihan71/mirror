47d593123830f2ec3fbde61fd309f2878c11e0d8
Mobashir Monim <mobashir@techynaf.com>
2018-10-01 16:14:09 +0600

    Fix music page musics

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -9,26 +9,30 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-12 p-4">
-                <div class="row pt-3">
-                    <div class="col-md-4">
-                        @foreach ($musics as $music)
-                            <div class="embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" width="560" height="315" src="{!!$music->url!!}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                            </div>
-                            <p class="media-text">{{$music->name}}</p>
-        
-                            @if (auth()->user() != null)
-                                @if (!App\Http\Controllers\Controller::notAdmin())
-                                    <a href="/music/delete/{{$music->id}}" class="btn btn-warning w-100">Delete</a>
-                                @endif
-                            @endif
-                        @endforeach
+            @foreach ($musics as $music)
+                @if ($loop->index % 3 == 0 && $loop->index != 0)
                     </div>
+                    <div class="row">
+                @endif
+                <div class="col-md-4 p-3">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" width="560" height="315" src="{!!$music->url!!}"></iframe"
+                            frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    </div>
+                    <p class="media-text">{{$music->name}}</p>
+
+                    @if (auth()->user() != null)
+                        @if (!App\Http\Controllers\Controller::notAdmin())
+                            <a href="/music/delete/{{$music->id}}" class="btn btn-warning w-100">Delete</a>
+                        @endif
+                    @endif
                 </div>
-            </div>
+            @endforeach
         </div>
+
+        <div class="text-center">{{ $musics->links() }}</div>
         <br>
+        
         <div class="row">
             <div class="col-md-2"></div>
             <div class="col-md-8">
