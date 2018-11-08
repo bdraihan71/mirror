4df1af5a5dc506130fb869ada6ba829cc8027ddc
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2018-11-08 19:20:18 +0600

    Fix error

--- app/Mail/VerifyMail.php
+++ app/Mail/VerifyMail.php
@@ -29,6 +29,6 @@ class VerifyMail extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.verifyUser')->with('user', $user);
+        return $this->view('emails.verifyUser')->with('user', $this->user);
     }
 }
