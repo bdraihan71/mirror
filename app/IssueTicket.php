eaa9f95044f79a0abcf0c656a0d1d47a25c2baf1
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2018-11-13 19:51:57 +0600

    Make IssueTickets fillable

--- app/IssueTicket.php
+++ app/IssueTicket.php
@@ -7,6 +7,8 @@ use App\Event;
 
 class IssueTicket extends Model
 {
+    protected $fillable = ['event_id', 'email', 'name', 'company', 'designation', 'phone', 'present'];
+
     public function event ()
     {
         return $this->belongsTo('App\Event');
