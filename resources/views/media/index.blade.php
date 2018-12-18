4ef69ada61d1ac7f96e08413e85ead44f2c43248
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2018-12-18 16:48:08 +0600

    Add album edit button

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -12,9 +12,16 @@
     <div class="row pt-3">
         @foreach ($albums as $album)
             @if (count($album->photos) != 0)
-                <div class="col-md-2">
+                <div class="col-md-2 image-block">
                     <a href="/album/show/{{$album->id}}"><img class="media-img" src="{{$album->photos[0]->url}}" alt="{{$album->photos[0]->caption}}" id="{{'myImg'.$album->photos[0]->id}}" onclick="modalImage({{$album->photos[0]->id}})"></a>
                     <p class="media-text">{{$album->name}}</p>
+                    @if (auth()->user() != null)
+                        @if (auth()->user()->role == 'admin' || auth()->user()->role == 'super-admin')
+                            <div class="edit">
+                                <a href="/media/photo/edit?album={{$album->id}}"><i class="fa far fa-edit float-right"></i></a>
+                            </div>
+                        @endif
+                    @endif
                 </div>
             @endif
         @endforeach
@@ -23,6 +30,7 @@
     <div class="text-center">{{ $albums->links() }}</div>
     @if (auth()->user() != null)
         @if (!App\Http\Controllers\Controller::notAdmin())
+        <br>
             <div class="row">
                 <div class="col-md-12">
                     <a href="/album/create" class="btn black-button w-100">Add Photos</a>
