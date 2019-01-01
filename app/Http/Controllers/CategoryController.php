e826680d70b9720fd53d4b0e1202cde58a7fb32c
bdraihan71 <bdraihan71@gmail.com>
2019-01-01 14:40:10 +0600

    category update section has started

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -77,7 +77,8 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category = Categories::find($id);
+        return view('category/edit', compact('category'));
     }
 
     /**
