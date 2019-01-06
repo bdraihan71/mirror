43b306e38afec8cdd7e63317a22f8b453cce4949
bdraihan71 <bdraihan71@gmail.com>
2019-01-06 13:18:25 +0600

    mouse icon adjusted

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             ProfilesTableSeeder::class,
             ProductsTableSeeder::class,
             PartnersTableSeeder::class,
+            CategoriesTableSeeder::class,
         ]);
     }
 }
