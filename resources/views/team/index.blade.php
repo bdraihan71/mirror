11ae511e589b720c9b7a99b046d2d8aa3ccabb4e
Asif Arefin Bonny <bonny@techynaf.com>
2019-08-08 02:39:54 +0600

    Team section design done

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -1,30 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container team">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="jumbotron">
-                    <img width="200px" class="img-thumbnail" src="/frontend/img/tasnim.png">
-                    <p>
-                    
-                    </p>
-                    <h3>Tasnim Mortoza/CEO</h3>
-                    <p>
-                    contact me: tasnim@techynaf.com
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="jumbotron">
-                    <img width="200px" class="img-thumbnail" src="/frontend/img/prince.jpg">
-                    <p></p>
-                    <h3>Gulshan Jubaed Prince/CTO</h3>
-                    <p>
-                    contact me: prince@techynaf.com
-                    </p>
-                </div>
-            </div>
-        </div>
+<div class="container">
+
+    <div class="heading">
+        <h2>Meet Our Team</h2>
     </div>
+  
+    <div class="row team-row">
+  
+
+      <div class="col-md-2"></div>
+
+      <div class="col-md-4 col-sm-6 team-wrap">
+        <div class="team-member text-center">
+          <div class="team-img">
+            <img src="/frontend/img/tasnim.png" alt="">
+            <div class="overlay">
+              <div class="team-details text-center">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                </p>
+                <div class="socials mt-20">
+                  <a href="#"><i class="fab fa-facebook"></i></a>
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+                  <a href="#"><i class="fab fa-google-plus"></i></a>
+                  <a href="#"><i class="fa fa-envelope"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h5 class="team-title">Tasnim Mortoza</h5>
+          <span>CEO</span>
+        </div>
+      </div>
+      <!-- end team member -->
+  
+      <div class="col-md-4 col-sm-6 team-wrap">
+        <div class="team-member text-center">
+          <div class="team-img">
+            <img src="/frontend/img/prince.jpg" alt="">
+            <div class="overlay">
+              <div class="team-details text-center">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                </p>
+                <div class="socials mt-20">
+                  <a href="#"><i class="fab fa-facebook"></i></a>
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+                  <a href="#"><i class="fab fa-google-plus"></i></a>
+                  <a href="#"><i class="fa fa-envelope"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h5 class="team-title">Gulshan Jubaed Prince</h5>
+          <span>CTO</span>
+        </div>
+      </div>
+      <!-- end team member -->
+
+      <div class="col-md-2"></div>
+  
+    </div>
+  </div>
 @endsection
